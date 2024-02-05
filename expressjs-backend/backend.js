0a9aae84351263cc77fe8a3cd22257ae3d69e8b0
juliayee <jye13@calpoly.edu>
2024-02-04 21:22:10 -0800

    m

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 8000;
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 
 const users = [
     { username: 'bj', password: 'pass424', },
@@ -14,6 +15,7 @@ const users = [
 
 mongoose.connect('mongodb://localhost/CSC424', { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
